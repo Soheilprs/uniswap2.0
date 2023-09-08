008b65b518218ead85eaaa4268c50f0130634c5b
soheilprs <soheilparsaei19@protonmail.com>
2023-09-08 18:29:34 +0330

    init

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,11 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-// export const ROUTER_ADDRESS = "0xCcb023170b096da50Ff12353D010AB0F1b81B44c"; 
 export const ROUTER_ADDRESS = "0xCcb023170b096da50Ff12353D010AB0F1b81B44c"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/lENQQ9Ydzlkd6OCHLaR_urix7H4VPfzm",
+    [Goerli.chainId]: "",
   },
 };
